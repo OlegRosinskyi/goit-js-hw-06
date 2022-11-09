053102d950ec6fd4e6595b9e45f87772232dda06
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-09 08:27:23 +0200

    update task02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,5 +1,25 @@
-const listWithId = document.querySelector("#categories");
-console.log(listWithId);
-const lli = listWithId.querySelectorAll(".item");
-console.log(lli);
-console.log(lli[0]);
+const NavIdEl = document.querySelector("#categories");
+//console.log(NavIdEl);
+const getAllItemEl = NavIdEl.querySelectorAll(".item");
+//console.log(getAllItemEl);
+console.log('Number of categories: ', getAllItemEl.length);
+//const getWonHederEl = getAllItemEl[1].querySelector("h2");
+//console.log(getWonHederEl.textContent);
+//console.log(getWonHederEl);
+const getAllHeadEl = NavIdEl.querySelectorAll("h2");
+//console.log(getAllHeadEl);
+
+//const getAllLiHederEl = getAllItemEl[0].querySelectorAll("li");
+//console.log(getWonHederEl.textContent, getAllLiHederEl.length);
+//console.dir(window);
+//console.dir(document); 
+for (let i = 0; i < getAllHeadEl.length; i += 1) {
+    console.log('Category:', getAllItemEl[i].querySelector("h2").textContent);
+    console.log('Elements:', getAllItemEl[i].querySelectorAll("li").length);
+  //  for (let j = 0; j < getAllLiHederEl[i].querySelectorAll("li").length; j += 1) {
+  //  console.log('Elements:', getAllHeadEl[j].querySelectorAll("li").length);
+  // };
+};
+//for (let i = 0; i < array.length; i += 1) {
+
+//}
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,89 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const NavIdEl = document.querySelector("#ingredients");
+const lastItem = document.createElement("li");
+const arrayLiElements = ingredients
+  .map((ingredient) => {
+    const lastItem = document.createElement("li");
+    lastItem.classList.add('item')
+    lastItem.textContent = ingredient;
+    return lastItem;
+  });
+NavIdEl.append(...arrayLiElements);
+  
+
+//--------------------------------------------------------------------------
+//const NavIdEl = document.querySelector("#ingredients");
+//const getEl = (teg, sel) => {
+ // const lastItem0 = document.createElement("li");
+//lastItem0.classList.add('item')
+//lastItem0.textContent = ingredients[0];
+//  };
+//------------------------------------------------------------------------------
+//const NavIdEl = document.querySelector("#ingredients");
+//const lastItem0 = document.createElement("li");
+//lastItem0.classList.add('item')
+//lastItem0.textContent = ingredients[0];
+
+//const lastItem1 = document.createElement("li");
+//lastItem1.classList.add('item')
+//lastItem1.textContent = ingredients[1];
+
+//const lastItem2 = document.createElement("li");
+//lastItem2.classList.add('item')
+//lastItem2.textContent = ingredients[2];
+
+//const lastItem3 = document.createElement("li");
+//lastItem3.classList.add('item')
+//lastItem3.textContent = ingredients[3];
+
+//const lastItem4 = document.createElement("li");
+//lastItem4.classList.add('item')
+//lastItem4.textContent = ingredients[4];
+
+//const lastItem5 = document.createElement("li");
+//lastItem5.classList.add('item')
+//lastItem5.textContent = ingredients[5];
+
+//NavIdEl.append(lastItem0, lastItem1, lastItem2, lastItem3, lastItem4, lastItem5);
+//---------------------------------------------------------------------------------
+
+//const colors = [
+// 	{ label: "red", color: "#FF0000" },
+// 	{ label: "green", color: "#00FF00" },
+// 	{ label: "blue", color: "#0000FF" },
+// 	{ label: "yellow", color: "#FFFF00" },
+// ];
+
+// const colorButtonElements = colors.map(({ color, label }) => {
+// const colorButton = document.createElement("button");
+// colorButton.style.backgroundColor = color;
+// colorButton.style.margin = "px";
+
+// colorButton.textContent = label;
+
+// return colorButton;
+// });
+
+// body.append(...colorButtonElements);
+  
+//--------------------------------------------------------------------------------------
+//const NavIdEl = document.querySelector("#ingredients");
+// const lastItem = document.createElement("li");
+//  lastItem.classList.add('item')
+//  lastItem.textContent = ingredient;
+ // NavIdEl.append(lastItem);
+//const markup = ingredients
+//  .map((ingredient) => `<li class="item">${ingredient}</li>`) //`<li class="item">${ingredient}</li>`
+//  .join("");
+//NavIdEl.innerHTML = markup;
+//-------------------------------------------------------------------------------------
+//const NavIdEl = document.querySelector("#ingredients");
+//ingredients.forEach(ingredient => {
+// const lastItem = document.createElement("li");
+//  lastItem.classList.add('item')
+//  lastItem.textContent = ingredient;
+ // NavIdEl.append(lastItem);
+//});
