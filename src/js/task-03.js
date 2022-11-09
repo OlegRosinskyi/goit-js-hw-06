d6e5a852a9ef1be0bf82591792c167e4f44c25ef
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-09 13:04:48 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,17 +13,46 @@ const images = [
   },
 ];
 const NavUlEl = document.querySelector(".gallery");
-//const lastLiEl =  document.createElement("li");
-//const lastInLiImageEl = document.createElement("img");
-
-
 const lastInLiImageEl = images
-  .map(({ url, alt }) => `<li> <img src="${url}" alt="${alt}"></li >`)
-.join("");
-//const colorButton = document.createElement("button");
-//colorButton.style.backgroundColor = color;
-//colorButton.style.margin = "px";
-
-// colorButton.textContent = label;
-
+  .map(({ url, alt }) =>
+  `<li class = "item"> <img class="imgformat" src="${url}" alt="${alt}" width="450" ></li >`)
+  .join("");
 NavUlEl.insertAdjacentHTML('beforeend', lastInLiImageEl);
+
+NavUlEl.classList.add("list"); 
+//console.log(NavUlEl);
+const NavUlEll = document.querySelector(".gallery");
+const list = document.querySelector(".list");
+//console.log(list);
+list.style.background = "rgba(47, 48, 58, 0.4)";
+list.style.display = "Flex";  //display: 'flex';
+list.style.flexwrap = "wrap";
+list.style.gap = "30px";
+list.style.listStyle = "none";
+list.style.paddingTop = "24px";
+list.style.paddingLeft = "0";
+list.style.paddingRight = "0";
+list.style.paddingBottom = "24px";
+
+const imgformats = document.querySelectorAll(".imgformat");
+//console.log(imgformats);
+imgformats.forEach(imgformat => {
+  imgformat.style.width = "100%";
+  imgformat.style.height = "auto";
+});
+
+ paddingBottom: ;
+
+//width: 100%;
+//      height: auto;//list-style: none;
+
+//const colors = [
+ //	"red","green","blue","yellow" ];
+//const imagElements = document.querySelectorAll(".imgformat");
+//console.log(imagEl);
+//const setBorder = imagElements.map
+//elem.classList.add("img") 
+//const images = document.querySelector(".img");
+//display: flex;
+//    flex-wrap: wrap;
+//    gap: 30px;
