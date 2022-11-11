86258a1811b2addab17ff16a34b085b499cad2d3
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-11 21:39:37 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const widgetEl = document.querySelector('.widget');
+const colorEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+let colorSet = 0;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onWidgetElColor = (revent) => { colorSet = getRandomHexColor(); console.log(colorSet); bodyEl.style.background = colorSet; colorEl.textContent = colorSet; };
+
+widgetEl.addEventListener('click', onWidgetElColor)
+
